15ed806febdc16244ddb8f1e40b445f18ac5b0a7
jakjan95 <jakubjanusz95@gmail.com>
2021-06-11 23:55:28 +0200

    remove vowels little refactor

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -10,16 +10,11 @@ void removeVowels(std::vector<std::string>& words)
     constexpr std::array<char, 6> vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
 
     auto is_vowel = [&vowels](char character) {
-        return std::find(vowels.begin(), vowels.end(), character) != vowels.end();
+        return std::find(vowels.begin(), vowels.end(), std::tolower(character)) != vowels.end();
     };
 
     auto remove_vowel = [&](std::string& word) {
-        word.erase(
-            std::remove_if(word.begin(), word.end(),
-                [&](auto letter) {
-                    return is_vowel(std::towlower(letter));
-                }),
-            word.end());
+        word.erase(std::remove_if(word.begin(), word.end(), is_vowel), word.end());
     };
 
     std::for_each(words.begin(), words.end(), remove_vowel);
